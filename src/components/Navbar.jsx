5be76cfee7cd9c635450908d0291f0990c90e7b9
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-13 17:50:18 +0600

    build: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container, Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import { FitnessCenter } from "@mui/icons-material";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Stack direction="row">
+        <FitnessCenter />
+        <Link to="/">Home</Link>
+        <Link to="/excercise">Exercise</Link>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Navbar;
