2ed82eb68bbfa3b153d512914a77dc3f329d434a
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-13 17:37:05 +0600

    build: route in root

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import ExerciseDetails from "./components/ExerciseDetails";
+import Footer from "./components/Footer";
 
 const Root = () => {
   return (
-    <div >
-      Bismillahir Rahmanir Rahim
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetails />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
-export default Root
+export default Root;
