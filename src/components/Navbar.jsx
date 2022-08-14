6a02076b3a6910fc1f4d427b841ad0d01d0c6e0e
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-14 11:37:38 +0600

    build: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import { Container, Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 import { FitnessCenter } from "@mui/icons-material";
+import classes from "../styles/All.module.css";
 
 const Navbar = () => {
   return (
-    <Container>
-      <Stack direction="row">
+    <Container className={classes.navContainer}>
+      <Stack direction="row" gap={2}>
         <FitnessCenter />
-        <Link to="/" sx={{ }}>
+        <Link to="/" className={classes.navLink}>
           Home
         </Link>
-        <Link to="/excercise">Exercise</Link>
+        <Link
+          to="/excercise"
+          style={{
+            textDecoration: "none",
+            color: "#3A1212",
+          }}
+        >
+          Exercise
+        </Link>
       </Stack>
     </Container>
   );
