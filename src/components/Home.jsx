ba76abf5194badd9d35461eb24c02085d5a9b2ab
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-14 11:41:26 +0600

    build: 3 func child component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Container } from "@mui/material";
+import React from "react";
+import HeroBanner from "./HeroBanner";
+import SearchExercise from "./SearchExercise";
+import Exercises from "./Exercises";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container>
+      <HeroBanner />
+      <SearchExercise />
+      <Exercises />
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
