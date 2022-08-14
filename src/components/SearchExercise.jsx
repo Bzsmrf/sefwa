b10fc5f374578290d6509d7770ca78cb6894a646
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-14 16:30:01 +0600

    build: search term, fetch data & store to redux (#1)

--- src/components/SearchExercise.jsx
+++ src/components/SearchExercise.jsx
@@ -1,8 +1,22 @@
 import { Box, Button, Grid, TextField, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import classes from "../styles/All.module.css";
+import { fetchSearch } from "../services/slices/searchSlice";
+import { useDispatch } from "react-redux";
 
 const SearchExercise = () => {
+  const [term, setTerm] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (term !== "") {
+      dispatch(fetchSearch(term.toLowerCase()));
+      console.log(term);
+      setTerm("");
+    }
+  };
+
   return (
     <Box className={classes.searchExercise}>
       <Grid container justifyContent="center">
@@ -15,8 +29,10 @@ const SearchExercise = () => {
           <TextField
             placeholder="Search exercise..."
             className={classes.textField}
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
           />
-          <Button variant="contained" color="error">
+          <Button variant="contained" color="error" onClick={handleSearch}>
             Search
           </Button>
         </Grid>
