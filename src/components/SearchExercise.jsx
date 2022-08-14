099e08d910a8c665a6a3937d31dca50102964871
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-14 13:55:29 +0600

    build: exercise component

--- src/components/SearchExercise.jsx
+++ src/components/SearchExercise.jsx
@@ -1,7 +1,28 @@
+import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
+import classes from "../styles/All.module.css";
 
 const SearchExercise = () => {
-  return <div>SearchExercise</div>;
+  return (
+    <Box className={classes.searchExercise}>
+      <Grid container justifyContent="center">
+        <Grid item lg={3}>
+          <Typography variant="h4" textAlign="center" mb={5}>
+            Awesome Exercise You
+            <br />
+            Should Know
+          </Typography>
+          <TextField
+            placeholder="Search exercise..."
+            className={classes.textField}
+          />
+          <Button variant="contained" color="error">
+            Search
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default SearchExercise;
