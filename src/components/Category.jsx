927d0cafdad56774be947f58dad89d692fc5c2e4
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-16 12:21:38 +0600

    2022-08-15 03:40:23
    build: category component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Box, Paper, Stack, styled } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCategory, getCategory } from "../services/slices/categorySlice";
 
 const Category = () => {
-  return (
-    <div>Category</div>
-  )
-}
+  const dispatch = useDispatch();
+  const categories = useSelector(getCategory);
+  console.log(categories);
 
-export default Category
+  useEffect(() => {
+    dispatch(fetchCategory());
+  }, [dispatch]);
+
+  return (
+    <Box>
+      <Stack direction="row" spacing={2}>
+        {Object.keys(categories).length === 0 ? (
+          <Box>Loading...</Box>
+        ) : (
+          categories.map((category, index) => (
+            <Paper
+              key={index}
+              sx={{
+                width: "128px",
+                height: "128px",
+              }}
+            >
+              {category}
+            </Paper>
+          ))
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default Category;
