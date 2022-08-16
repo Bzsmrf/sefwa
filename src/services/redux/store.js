6689da733165134aa51f15ced8450c16194d5ee0
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-16 11:46:25 +0600

    feat: category reducer

--- src/services/redux/store.js
+++ src/services/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "../slices/searchSlice";
+import categoryReducer from "../slices/categorySlice";
 
 const store = configureStore({
   reducer: {
     search: searchReducer,
+    category: categoryReducer,
   },
 });
 export default store;
